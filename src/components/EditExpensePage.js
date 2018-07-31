48bf9fc4f6a9b3de0058fcae1844f32343ebb558
Lawrence <Lawrence4code@gmail.com>
2018-08-01 04:43:06 +0530

    Impleted Query string to the edit route for params and data management

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const EditExpensePage = () => {
-  return <div> This is from my edit expense component. </div>;
+const EditExpensePage = props => {
+  console.log(props);
+  return (
+    <div> This is from my edit expense with id: {props.match.params.id} . </div>
+  );
 };
 
 export default EditExpensePage;
