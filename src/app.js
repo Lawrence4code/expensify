a85735755163ebfe8f31a24092693a66b46aba3b
Lawrence <Lawrence4code@gmail.com>
2018-08-02 04:28:15 +0530

    Integrated redux and created folder structure to maintain separate files, committed after testing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+const store = configureStore();
+
+// addExpense -> water bill
+const expenseOne = store.dispatch(
+  addExpense({
+    description: 'water bill',
+    note: 'February water bill',
+    amount: 1000,
+    createdAt: -2000
+  })
+);
+
+// addExpense -> gas bill
+const expenseTwo = store.dispatch(
+  addExpense({
+    description: 'gas bill',
+    note: 'February gas bill',
+    amount: 2000,
+    createdAt: 3000
+  })
+);
+
+// setTextFilter  -> bill -> water
+
+store.dispatch(setTextFilter('bill'));
+
+// get visibleExpenses -> print value
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+// console.log(store.getState());
 
 ReactDOM.render(AppRouter, document.getElementById('app'));
---
+++ src/reducers/expenses.js
@@ -0,0 +1,29 @@
+// Expense Reducer
+const expensesReducerDefaultState = []; // incase of one to many default value, variable is created.
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  //  separate reducer for expenses
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+      return state.filter(exp => {
+        return exp.id !== action.id;
+      });
+    case 'EDIT_EXPENSE':
+      return state.map(expense => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          };
+        } else {
+          return state;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
+export default expensesReducer;
