7adca6ecf74b0fe379ae9afb4a7b06e53a8ea40d
Lawrence <Lawrence4code@gmail.com>
2018-08-09 06:28:50 +0530

    Setup formatting for amount and createdAT in ExpenseList

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = ({ id, description, amount, createdAt }) => {
   // this part is important, we can pass dispatch along with other information from redux, so no needs of mapStateToProps or props
@@ -10,7 +12,9 @@ export const ExpenseListItem = ({ id, description, amount, createdAt }) => {
       </Link>
 
       <p>
-        Amount: ${amount} - created on: {createdAt}
+        {numeral(amount / 100).format('$0,0.00')}
+        -
+        {moment(createdAt).format('MMM Do YYYY')}
       </p>
     </div>
   );
