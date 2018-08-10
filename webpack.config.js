dbc486415018ea0bd73f82dc15617fc900081a16
Lawrence <Lawrence4code@gmail.com>
2018-08-11 04:29:31 +0530

    Typo fix with webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
-const webpack = require(webpack);
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === 'developement') {
+} else if (process.env.NODE_ENV === 'development') {
   require('dotenv').config({ path: '.env.development' });
 }
 
@@ -38,22 +38,22 @@ module.exports = env => {
     plugins: [
       CSSExtract,
       new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stringfy(
+        'process.env.FIREBASE_API_KEY': JSON.stringify(
           process.env.FIREBASE_API_KEY
         ),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringfy(
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(
           process.env.FIREBASE_AUTH_DOMAIN
         ),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringfy(
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(
           process.env.FIREBASE_DATABASE_URL
         ),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringfy(
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(
           process.env.FIREBASE_PROJECT_ID
         ),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringfy(
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(
           process.env.FIREBASE_STORAGE_BUCKET
         ),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringfy(
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
           process.env.FIREBASE_MESSAGING_SENDER_ID
         )
       })
