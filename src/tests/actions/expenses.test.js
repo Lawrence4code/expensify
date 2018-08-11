23ceb9af4d6d6bc357db0070243e832e93271fb3
Lawrence <Lawrence4code@gmail.com>
2018-08-11 10:24:29 +0530

    Implement edit expense option, connected to firebase and created test case

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -5,7 +5,8 @@ import {
   removeExpense,
   setExpenses,
   startSetExpenses,
-  startRemoveExpense
+  startRemoveExpense,
+  startEditExpense
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import configureMockStore from 'redux-mock-store';
@@ -66,6 +67,29 @@ test('should setup edit expense action object', () => {
     id: '123abc',
     updates: { note: 'new note value' }
   });
+});
+
+test('should edit expense in firebase', done => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = {
+    amount: 21045
+  };
+  store
+    .dispatch(startEditExpense(id, updates))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: 'EDIT_EXPENSE',
+        id,
+        updates
+      });
+      return database.ref(`expenses/${id}`).once('value');
+    })
+    .then(snapshot => {
+      expect(snapshot.val().amount).toBe(updates.amount);
+      done();
+    });
 });
 
 // Add Expense test // case one: with data
