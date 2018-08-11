b72ce4874d7bc89c4559a0deb354a7beca1d199f
Lawrence <Lawrence4code@gmail.com>
2018-08-11 09:02:15 +0530

    Setup startRemoveExpense, intregrated with firebase  and test case created

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -46,6 +46,18 @@ export const removeExpense = ({ id } = {}) => {
   };
 };
 
+// START_REMOVE_EXPENSE
+export const startRemoveExpense = ({ id } = {}) => {
+  return dispatch => {
+    return database
+      .ref(`expenses/${id}`)
+      .remove()
+      .then(() => {
+        dispatch(removeExpense({ id }));
+      });
+  };
+};
+
 // EDIT_EXPENSE
 
 export const editExpense = (id, updates) => {
